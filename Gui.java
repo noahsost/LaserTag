05aaa17c020b534c8d3d642b5daa9459c56ecc4b
SpencerSmith777 <114552770+SpencerSmith777@users.noreply.github.com>
2023-02-13 14:13:16 -0600

    Update Gui.java
    Added buttons and text fields. Needs work graphically, but functions for taking in a name and adding them to a team

--- Gui.java
+++ Gui.java
@@ -2,6 +2,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
 import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -28,22 +30,60 @@ class Gui{
     }
 
     void playerEntry(){
-        
+        //defining locations in relation to each object
         GridBagConstraints constraint = new GridBagConstraints();
-        constraint.gridx = 1;
+        constraint.gridx = 0;
         constraint.gridy = 0;
-
+        GridBagConstraints constraintR = new GridBagConstraints();
+        constraint.gridx = 0;
+        constraint.gridy = 1;
+        GridBagConstraints constraintB = new GridBagConstraints();
+        constraint.gridx = 2;
+        constraint.gridy = 1;
+        GridBagConstraints constraintRT = new GridBagConstraints();
+        constraint.gridx = 0;
+        constraint.gridy = -1;
+        GridBagConstraints constraintBT = new GridBagConstraints();
+        constraint.gridx = 8;
+        constraint.gridy = -1;
+        JButton redButton= new JButton("Red Team"); 
+        JButton blueButton= new JButton("Blue Team"); 
         // when the column value is changed, it changes the width of 
         JTextField textField = new JTextField(20);
+        // making the areas where teams are displayed, defining size and labelling
+        JTextArea textAreaR = new JTextArea(20,20);
+        textAreaR.append("RED TEAM");
+        textAreaR.setEditable(false);
+        JTextArea textAreaB = new JTextArea(20,20);
+        textAreaB.append("BLUE TEAM");
+        textAreaB.setEditable(false);
+        //adding elements to the screen
         this.frame.add(textField, constraint);
+        this.frame.add(redButton, constraintR);
+        this.frame.add(blueButton, constraintB);
+        this.frame.add(textAreaR, constraintRT);
+        this.frame.add(textAreaB, constraintBT);
         this.frame.setVisible(true);
-        textField.addActionListener(new ActionListener(){
-            // when enter is pressed, the text in the text box is recorded and removed
+        redButton.addActionListener(new ActionListener(){
+            // when RedButton is pressed, the text in the text box is recorded and moved to the Red team
             @Override
             public void actionPerformed(ActionEvent event) {
                 // records text in aString
                 String aString = textField.getText();
-                System.out.println(aString);
+                // moves text to Red team area
+                textAreaR.append("\n"+aString);
+                // empties the text box
+                textField.setText(null);
+            }
+        });
+        blueButton.addActionListener(new ActionListener(){
+            // when BlueButton is pressed, the text in the text box is recorded and moved to the Blue team
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                // records text in aString
+                String aString = textField.getText();
+                // moves text to Blue team area
+                textAreaB.append("\n"+aString);
                 // empties the text box
                 textField.setText(null);
             }
